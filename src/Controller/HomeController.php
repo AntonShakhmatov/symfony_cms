9224ec7cb1cfd22ebbab8efd5ca563cfa62a5e20
Antoha <xshakhmatov@gmail.com>
2025-01-15 01:29:33 +0100

    menu commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\MenuRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(MenuRepository $menuRepository): Response
     {
+
+        // Получение данных из базы
+        $posts = $menuRepository->findBy([], ['position' => 'ASC']);
 
         $projects = [
             ['image' => 'images/project-item-01.jpg', 'title' => 'Best Design', 'description' => 'Lorem ipsum dolor...'],
@@ -19,9 +24,25 @@ class HomeController extends AbstractController
             // Добавь остальные проекты по аналогии
         ];
 
+        $host = $_SERVER['HTTP_HOST'];
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'posts' => $posts,
             'projects' => $projects,
+            'host' => $host
         ]);
     }
+
+    // public function getMenu(MenuRepository $menuRepository): Response
+    // {
+    //     // Получение данных из базы
+    //     $posts = $menuRepository->findBy([], ['createdAt' => 'ASC']);
+
+    //     // Передача данных в шаблон
+    //     return $this->render('home/index.html.twig', [
+    //         'controller_name' => 'HomeController',
+    //         'posts' => $posts,
+    //     ]);
+    // }
 }
